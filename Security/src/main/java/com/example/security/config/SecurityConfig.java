3b34e0f856791bb2337dd872f75f3d9cbf9c8164
djordjeprzulj <przulj@gmail.com>
2018-04-19 01:01:26 +0200

    In memory autentifikacija.

--- Security/src/main/java/com/example/security/config/SecurityConfig.java
+++ Security/src/main/java/com/example/security/config/SecurityConfig.java
@@ -1,16 +1,48 @@
 package com.example.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	//	Basic authentication, zahteva se autentifikacija za svaku stranicu, dozvoljen pristup bez obzira na uloge.	 
-	protected void configure(HttpSecurity http)throws Exception {
+	/*
+	//	Basic authentication, zahteva se autentifikacija za svaku stranicu, dozvoljen pristup samo home
+	//	ne uzima se u obzir autorizacija
+	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests().antMatchers("/**").permitAll()
-			.and().httpBasic().and().csrf().disable();
+			.authorizeRequests().antMatchers("/", "/home").permitAll()
+			.anyRequest().authenticated()
+			.and().httpBasic()	// login browsera
+			.and().csrf().disable();
+	}
+	*/
+/*
+	// in memory autentifikacija
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests().antMatchers("/", "/home").permitAll()
+				.anyRequest().authenticated()
+//				.and()
+//			.formLogin()
+//				.loginPage("/login")
+//				.permitAll()
+//				.and()
+//			.logout()
+//				.permitAll()
+				.and().httpBasic();
+	}
+	*/
+	@Bean
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+		return manager;
 	}
 }
